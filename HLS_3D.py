538f3d482d8123f3f32a887ea4a2e73cbd9759fa
Rio Fujimiya <r-2238@outlook.com>
2023-04-22 22:19:16 +0900

    minor revision

--- HLS_3D.py
+++ HLS_3D.py
@@ -1,7 +1,33 @@
+#!/usr/bin/env python3
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import ImageGrab, Image
-import math, os, io, colorsys, re
+import math, os, io, colorsys
+
+### config ###
+
+# Target number of points to plot
+target_point_count = 2**12
+
+# Size (0 to hide) and quality of the image displayed in the plot
+prevsize, img_quality = 0.3, 512
+
+# Save png or not and where to save it
+export_png, export_directry = True, ".\HLSphere"
+
+# Save rotaing animation, convert into mp4 movie, animation length, fps
+export_pngs, convert_to_mp4, rotation_azims_count, mp4_fps = False, True, 240, 60
+
+# Resolution of images to save
+export_dpi = 240
+
+# Show manipulable plot
+show_plot = True
+
+# Ask for retry on finish
+retry_always, retry_on_error = False, True
+
+### config ###
 
 
 def get_image():
@@ -11,19 +37,18 @@ def get_image():
     description = f.getvalue()
 
     if isinstance(img, Image.Image):
-        print(f"clipboard:{img}")
+        print(f"image on clipboard>>{img}")
     else:
         f = io.StringIO()
         print(img, file=f)
         try:
             filepath = f.getvalue().split("'")[1]
             img = Image.open(filepath)
-            print(f"clipboard:{filepath}")
         except:
             print("no image on clipboard")
 
         if isinstance(img, Image.Image):
-            print(filepath)
+            print(f"image file on clipboard>>{filepath}")
             description = filepath
         else:
             files = sorted(os.listdir(), key=lambda x: os.stat(x).st_mtime)
@@ -31,7 +56,7 @@ def get_image():
                 try:
                     img = Image.open(f)
                     if isinstance(img, Image.Image):
-                        print(f"latest image in dir:{f}")
+                        print(f"latest image in directry>>{f}")
                         description = f
                         break
                 except:
@@ -47,8 +72,8 @@ def img_to_rgb(img):
 
 
 def init_plot(title: str = ""):
-    plt.style.use("fast")
-    fig = plt.figure(f"[HLS sphere] {title}")
+    # plt.style.use("fast")
+    fig = plt.figure(f"[HLSphere] {title}", figsize=(8, 4.5), dpi=export_dpi)
     ax = fig.add_subplot(111, projection="3d")
     ax.view_init(elev=30, azim=150)
     xylim = 0.7
@@ -91,13 +116,13 @@ def draw_H_ring(ax):
     for i in range(ring_counts):
         ringc[i] = colorsys.hls_to_rgb(h[i], l[i], s[i])
     rx, ry, rz = solveHLSxyz(ringc)
-    ax.scatter(rx, ry, rz, color=ringc, alpha=1, s=3, marker=".")
+    ax.scatter(rx, ry, rz, color=ringc, alpha=1, s=1, marker=".")
     return ax
 
 
 def draw_points(ax, rgb):
-    target_point_count = 2**12
-    spd = math.ceil((height + width) / (target_point_count**0.5) / 2)
+    height, width = len(rgb), len(rgb[0])
+    spd = math.floor((height + width) / (target_point_count**0.5) / 2)
 
     white = [1, 1, 1]
     points = np.array([white])
@@ -108,14 +133,16 @@ def draw_points(ax, rgb):
             pcolor = rgb[rp, cp] / 255
             if not np.allclose(pcolor, white):
                 points = np.append(points, [pcolor], axis=0)
-    print(f"plot points:{len(points)}")
+            strBar = ("X" * int(len(points) / target_point_count * 20)) + ("_" * 20)
+            print(f"plotting:{strBar[:20]}\r", end="")
+    print(f"\nplot points:{len(points)}")
 
     x, y, z = solveHLSxyz(points)
-    ax.scatter(x, y, z, color=points, alpha=1, s=100, marker=".")
+    ax.scatter(x, y, z, color=points, alpha=1, s=50, marker=".")
     return ax
 
 
-if __name__ == "__main__":
+def HLS_sphere():
     img, description = get_image()
     if img:
         rgb, height, width = img_to_rgb(img)
@@ -124,30 +151,73 @@ if __name__ == "__main__":
         draw_H_ring(ax)
         draw_points(ax, rgb)
 
-        prevsize = 0.4
-        plt.axes([0, 0, prevsize, prevsize * width / height])
-        plt.imshow(img)
-        plt.axis("off")
+        if prevsize > 0:
+            diag = ((width**2) + (height**2)) ** 0.5
+            img_resize = img.resize(
+                (int(img_quality / diag * width), int(img_quality / diag * height))
+            )
 
-        if 0:  # <- true:png->mp4 / false:show
-            azims = 180
-            frames = []
-            safe_description = re.sub(r'[\\|/|:|?|.|"|<|>|\|]', "-", description)
-            for i in range(azims * 2):
-                ax.view_init(
-                    elev=15 + 30 * math.cos(math.pi * (i) / azims), azim=i * 360 / azims
-                )
-                frame = f"HSLsphere_{safe_description}_{i:03}.png"
-                frames.append(frame)
-                plt.savefig(frame, dpi=300)
-                print(f"draw frame {i:03}/{azims*2}")
-            import pngs2mp4
+            plt.axes([0, 0, prevsize, prevsize])
+            plt.imshow(img_resize)
+            plt.axis("off")
 
-            pngs2mp4.pngs2mp4(frames, f"HSLsphere_{safe_description}")
-            for f in frames:
-                os.remove(f)
+        if export_png or export_pngs:
+            import re
 
-        else:
+            safe_description = re.sub(
+                r'[\\|/|:|?|.|"|<|>|\||=| ]', "-", description
+            ).replace("\n", "")
+            filename_prefix = "HLSphere"
+
+            os.makedirs(export_directry, exist_ok=True)
+            filename_body = (
+                export_directry + os.sep + f"{filename_prefix}_{safe_description}"
+            )
+
+            if export_png:
+                pngfile = filename_body + ".png"
+                plt.savefig(pngfile, dpi=export_dpi)
+                print("exported>>" + pngfile)
+
+            if export_pngs:
+                frames = []
+                for i in range(rotation_azims_count * 2):
+                    ax.view_init(
+                        elev=15 + 30 * math.cos(math.pi * (i) / rotation_azims_count),
+                        azim=i * 360 / rotation_azims_count,
+                    )
+                    frame = filename_body + f"_{i:05}.png"
+                    frames.append(frame)
+                    plt.savefig(frame, dpi=export_dpi)
+                    print(f"exported>>{frame}/{rotation_azims_count*2}\r", end="")
+                print()
+
+                if convert_to_mp4:
+                    import pngs2mp4
+
+                    pngs2mp4.pngs2mp4(frames, filename_body, mp4_fps)
+                    for f in frames:
+                        os.remove(f)
+
+        if show_plot:
             plt.show()
+
+        return 1
     else:
-        print(">>>FAILED to get image<<<")
+        print(
+            "FAILED to get image\ncopy image to clipboard or put image file in the same directry"
+        )
+        return 0
+
+
+if __name__ == "__main__":
+    retry = True
+    print(
+        "============================\nHLS sphere plotter v0.1.1\n(c)Rio Fujimiya|i2forme 2023\n============================"
+    )
+    while retry:
+        if (not HLS_sphere() == 1 and retry_on_error) or retry_always:
+            retry = str(input("Retry?[Enter]")) == ""
+        else:
+            retry = False
+            print("FINISHED")
